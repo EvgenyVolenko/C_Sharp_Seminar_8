cf31b35d21d957b258c674363d8871395883f701
Evgeny <evolenko@inbox.ru>
2022-12-15 23:37:43 +0300

    Added Task_061

---
+++ Task061CW_Triangle_Paskal/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 61: Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника
+
+Console.Clear();
+int digit;
+
+Console.Write("Введите колличество строк треугольника паскаля: ");
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out digit)) break;
+    else Console.Write("Ну просил же ЧИСЛО: ");
+}
+
