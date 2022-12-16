ce5711a1f2f5231eaa224dd54a2e8cabfb88a225
Evgeny <evolenko@inbox.ru>
2022-12-16 17:48:39 +0300

    Added Working Task060

---
+++ Task060HW_3D_Array/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+Console.Clear();
+
+int[] digits = { 12, 13, 14, 15, 18, 20, 36, 35 };
+int[,,] array3D = CreateMatrix(digits, 2, 2, 2);
+PrintMatrix(array3D);
+
+int[,,] CreateMatrix(int[] colection, int row, int col, int dep)
+{
+    int[,,] matrix = new int[row, col, dep];
+    int n = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = colection[n];
+                n++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
